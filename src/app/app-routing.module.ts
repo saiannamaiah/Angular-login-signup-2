4804902260bbb0f5c2922e879372cab4f70fa7a1
saiannamaiah <annamaiahsai@gmail.com>
2023-09-20 15:10:52 +0530

    New Login Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  {path:'auth', component: AuthComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
